e622fb37d232566771d6dd1cb373e68e5e8b4c94
Platon Rogovskoy <88965118+rogovskoy@users.noreply.github.com>
2026-01-16 15:04:40 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,27 +16,29 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Boot control HAL
+# Boot control HAL (shared, без PRODUCT_STATIC_BOOT_CONTROL_HAL)
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl-1.2-mtkimpl \
-    android.hardware.boot@1.2-service
+    android.hardware.boot@1.2-service \
+    bootctrl.mt6768
 
+# Health HAL
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl-2.1 \
     android.hardware.health@2.1-service \
     libhealthd.$(PRODUCT_PLATFORM)
 
+# Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mtk \
     fastbootd
 
-PRODUCT_PACKAGES += \
-    bootctrl.mt6768
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6768 \
-    libz \
-    libcutils
+# че за хуню ты написал блин
+# УДАЛЕНО, ЧТОБЫ ПОФИКСИТЬ ОШИБКУ:
+# PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+#     bootctrl.mt6768 \
+#     libz \
+#     libcutils
 
 PRODUCT_PACKAGES += \
     cppreopts.sh \
@@ -58,7 +60,6 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
-
 
 # Keymaster
 PRODUCT_PACKAGES += \
